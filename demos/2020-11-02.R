5938a4e9aae4379723f7cfe852207c43486db69f
Toby Dylan Hocking <tdhock5@gmail.com>
2020-11-02 06:30:31 -0700

    pca hw

---
+++ demos/2020-11-02.R
@@ -0,0 +1,11 @@
+## 0. iris data scatterplot two columns with one principal component line
+##    and orthogonal projection segments.
+
+## 1. MNIST scatterplot of digit labels on first two pca directions (all
+## classes).
+
+## 2. for one class first two pca directions, sample grid. (ESL Fig 14.23)
+
+## 3. mean and first two eigendigits. reconstruction % and number of
+##    components in different panels for a single example. (MLAPP Fig
+##    12.6)
